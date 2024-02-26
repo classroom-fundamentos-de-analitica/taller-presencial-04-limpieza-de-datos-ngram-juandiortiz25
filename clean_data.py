3ea8122590d91f1a6f72f0a53b07b2eb41d175d3
Juan Diego Ortiz <juortizt@unal.edu.co>
2024-02-26 11:26:34 -0500

    Update clean_data.py

--- clean_data.py
+++ clean_data.py
@@ -6,77 +6,75 @@ import pandas as pd
 def load_data(input_file):
 """Lea el archivo usando pandas y devuelva un DataFrame"""
 
-df = pd.read_csv(input_file)
-return df
+    df = pd.read_csv(input_file)
+    return df
 
 
 def create_key(df, n):
 """Cree una nueva columna en el DataFrame que contenga el key de la columna 'text'"""
+    df = df.copy()
 
-df = df.copy()
+    # Copie la columna 'text' a la columna 'key'
+    df["key"] = df["text"]
 
-# Copie la columna 'text' a la columna 'key'
-df["key"] = df["text"]
+    # Remueva los espacios en blanco al principio y al final de la cadena
+    df["key"] = df["key"].str.strip()
 
-# Remueva los espacios en blanco al principio y al final de la cadena
-df["key"] = df["key"].str.strip()
+    # Convierta el texto a minúsculas
+    df["key"] = df["key"].str.lower()
 
-# Convierta el texto a minúsculas
-df["key"] = df["key"].str.lower()
+    # Transforme palabras que pueden (o no) contener guiones por su version sin guion.
+    df["key"] = df["key"].str.replace("-", "")
 
-# Transforme palabras que pueden (o no) contener guiones por su version sin guion.
-df["key"] = df["key"].str.replace("-", "")
+    # Remueva puntuación y caracteres de control
+    df["key"] = df["key"].str.translate(
+    str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+    )
 
-# Remueva puntuación y caracteres de control
-df["key"] = df["key"].str.translate(
-str.maketrans("", "", "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
-)
+    # Convierta el texto a una lista de tokens
+    df["key"] = df["key"].str.split()
 
-# Convierta el texto a una lista de tokens
-df["key"] = df["key"].str.split()
+    # Una el texto sin espacios en blanco
+    df["key"] = df["key"].str.join("")
 
-# Una el texto sin espacios en blanco
-df["key"] = df["key"].str.join("")
+    # Convierta el texto a una lista de n-gramas
+    df["key"] = df["key"].map(lambda x: [x[t:t+n-1] for t in range(len(x))])
 
-# Convierta el texto a una lista de n-gramas
-df["key"] = df["key"].map(lambda x: [x[t:t+n-1] for t in range(len(x))])
+    # Ordene la lista de n-gramas y remueve duplicados
+    df["key"] = df["key"].apply(lambda x: sorted(set(x)))
 
-# Ordene la lista de n-gramas y remueve duplicados
-df["key"] = df["key"].apply(lambda x: sorted(set(x)))
+    # Convierta la lista de ngramas a una cadena
+    df["key"] = df["key"].str.join("")
 
-# Convierta la lista de ngramas a una cadena
-df["key"] = df["key"].str.join("")
-
-return df
+    return df
 
 
 def generate_cleaned_column(df):
 """Crea la columna 'cleaned' en el DataFrame"""
 
-df = df.copy()
+    df = df.copy()
 
-# Ordene el dataframe por 'key' y 'text'
-df = df.sort_values(by=["key", "text"], ascending=[True, True])
+    # Ordene el dataframe por 'key' y 'text'
+    df = df.sort_values(by=["key", "text"], ascending=[True, True])
 
-# Seleccione la primera fila de cada grupo de 'key'
-keys = df.drop_duplicates(subset="key", keep="first")
+    # Seleccione la primera fila de cada grupo de 'key'
+    keys = df.drop_duplicates(subset="key", keep="first")
 
-# Cree un diccionario con 'key' como clave y 'text' como valor
-key_dict = dict(zip(keys["key"], keys["text"]))
+    # Cree un diccionario con 'key' como clave y 'text' como valor
+    key_dict = dict(zip(keys["key"], keys["text"]))
 
-# Cree la columna 'cleaned' usando el diccionario
-df["cleaned"] = df["key"].map(key_dict)
+    # Cree la columna 'cleaned' usando el diccionario
+    df["cleaned"] = df["key"].map(key_dict)
 
-return df
+    return df
 
 
 def save_data(df, output_file):
-"""Guarda el DataFrame en un archivo"""
-
-df = df.copy()
-df = df[["cleaned"]]
-df = df.rename(columns={"cleaned": "text"})
-df.to_csv(output_file, index=False)
+    
+    df = df.copy()
+    df = df[["cleaned"]]
+    df = df.rename(columns={"cleaned": "text"})
+    df.to_csv(output_file, index=False)
 
 
 def main(input_file, output_file, n=2):
